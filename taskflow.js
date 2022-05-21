9fd8e8bae6d72ef2fb0f4c4630ee3c6fb58363ec
Gitesh <gitesh.khodiyar@gmail.com>
2022-05-21 16:07:37 +0100

    upload CSV Label,Input double click bug fixed

--- taskflow.js
+++ taskflow.js
@@ -369,6 +369,11 @@ function clkSettings(){
   alert("stub for settings");
 };
 
+////////
+//
+// Upload CSV into local storage
+//
+////////
 
 function clkUploadTasksToLocalStorage(){
   console.log("CLICKED - stub for file upload")
@@ -378,15 +383,79 @@ function clkUploadTasksToLocalStorage(){
   readFile = function () {
       var reader = new FileReader();
       reader.onload = function () {
-          document.getElementById('uploadCSVOutput').innerHTML = reader.result;
+        
+        var uploadedData = convertCSVtoJSON(reader.result);
+        
+        //console.log(uploadedData);  
+
+        // document.getElementById('uploadCSVOutput').innerHTML = reader.result;
       };
       // start reading the file. When it is done, calls the onload event defined above.
       reader.readAsBinaryString(fileInput.files[0]);
+
+
   };
 
 fileInput.addEventListener('change', readFile);
 
+
 }
+
+
+function convertCSVtoJSON(thisFile){
+
+  csv = thisFile;
+
+  console.log("TASKFLOW CSV: ",csv);
+  
+  // const fs = require("fs");
+  // csv = fs.readFileSync("fileInput");
+  
+  const array = csv.toString().split("\n");
+  
+  /* Store the converted result into an array */
+  const csvToJsonResult = [];
+  
+  /* Store the CSV column headers into seprate variable */
+  const headers = array[0].split(", ")
+  
+  /* Iterate over the remaning data rows */
+  for (let i = 1; i < array.length - 1; i++) {
+  /* Empty object to store result in key value pair */
+  const jsonObject = {}
+  /* Store the current array element */
+  const currentArrayString = array[i]
+  let string = ''
+  
+  let quoteFlag = 0
+  for (let character of currentArrayString) {
+    if (character === '"' && quoteFlag === 0) {
+        quoteFlag = 1
+    }
+    else if (character === '"' && quoteFlag == 1) quoteFlag = 0
+    if (character === ', ' && quoteFlag === 0) character = '|'
+    if (character !== '"') string += character
+  }
+  
+  let jsonProperties = string.split("|")
+  
+  for (let j in headers) {
+    if (jsonProperties[j].includes(", ")) {
+    jsonObject[headers[j]] = jsonProperties[j]
+      .split(", ").map(item => item.trim())
+    }
+    else jsonObject[headers[j]] = jsonProperties[j]
+  }
+  /* Push the genearted JSON object to resultant array */
+  csvToJsonResult.push(jsonObject)
+  }
+  /* Convert the final array to JSON */
+  const json = JSON.stringify(csvToJsonResult);
+
+  console.log("TASKFLOW JSON ::::::::::", json)
+    
+}
+
 
 
 
